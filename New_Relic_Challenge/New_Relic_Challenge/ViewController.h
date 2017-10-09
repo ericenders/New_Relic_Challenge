9e90d6eff199b225984e1262b6302cbc5abc06c8
ericenders <eric.enders1@gmail.com>
2017-10-08 17:32:07 -0700

    Add files via upload

--- New_Relic_Challenge/New_Relic_Challenge/ViewController.h
+++ New_Relic_Challenge/New_Relic_Challenge/ViewController.h
@@ -10,12 +10,18 @@
 
 @interface ViewController : UIViewController <UITableViewDelegate,UITableViewDataSource>
 {
-    IBOutlet UITableView *tableView;
-    
+    IBOutlet UITableView *tableView1;
+    IBOutlet UITableView *tableView2;
 }
- @property (strong, nonatomic) IBOutlet UITextField *textField;
- @property (strong, nonatomic) IBOutlet UILabel *labelCount;
-@property (strong, nonatomic) NSMutableArray *nr_mut_array;
+@property (strong, nonatomic) IBOutlet UITextField *textField;
+@property (strong, nonatomic) IBOutlet UILabel *list1LabelCount;
+@property (strong, nonatomic) IBOutlet UILabel *list2LabelCount;
+@property (strong, nonatomic) IBOutlet UIButton *removeButton;
+@property (strong, nonatomic) IBOutlet UIButton *replaceButton;
+@property (strong, nonatomic) IBOutlet UIButton *addBeforeButton;
+@property (strong, nonatomic) IBOutlet UIButton *addAfterButton;
+@property (strong, nonatomic) NSMutableArray *nr_mut_array_list1;
+@property (strong, nonatomic) NSMutableArray *nr_mut_array_list2;
 - (IBAction) clearList;
 - (IBAction) addToList;
 - (IBAction) removeRow;
@@ -23,5 +29,7 @@
 - (IBAction) addBefore;
 - (IBAction) addAfter;
 - (IBAction) shuffleArray;
+- (IBAction) sortArray;
+- (IBAction) mergeLists;
 @end
 
--- New_Relic_Challenge/New_Relic_Challenge/ViewController.m
+++ New_Relic_Challenge/New_Relic_Challenge/ViewController.m
@@ -12,32 +12,91 @@
 @end
 
 @implementation ViewController
-@synthesize textField,labelCount;
+@synthesize textField,list1LabelCount,list2LabelCount,removeButton,replaceButton,addAfterButton,addBeforeButton;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //textView.editable = NO;
     [self refreshArray];
- //   [self addToView];
-   tableView.delegate = self;
-   tableView.dataSource = self;
-  
-    }
-
-- (void) addToView {
-    @synchronized(self){
-        NSMutableString *string = [NSMutableString string];
-        for( NSObject *myObject in self.nr_mut_array) {
-            [string appendString:[NSString stringWithFormat:@"%@\n", myObject]];
-        }
-       // tableView = string;
-        labelCount.text = [NSString stringWithFormat:@"Items(%lu)",(unsigned long)[self.nr_mut_array count]];
-        [tableView reloadData];
+    tableView1.delegate = self;
+    tableView1.dataSource = self;
+    tableView2.delegate = self;
+    tableView2.dataSource = self;
+    
 }
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
 }
+- (void) refreshArray {
+    //Allocate and initialize the NSMutableArray
+    self.nr_mut_array_list1 = [[[NSMutableArray alloc] init]autorelease]; //auto release since ARC is turned off
+    self.nr_mut_array_list2 = [[[NSMutableArray alloc] init]autorelease];
+    
+    //Initial Data for the first list
+    [self.nr_mut_array_list1 addObject:@"Eric Enders"];
+    [self.nr_mut_array_list1 addObject:@"is"];
+    [self.nr_mut_array_list1 addObject:@"perfect"];
+    [self.nr_mut_array_list1 addObject:@"for"];
+    [self.nr_mut_array_list1 addObject:@"this"];
+    [self.nr_mut_array_list1 addObject:@"role"];
+    //Update Count method to update the label with the list count
+    [self updateCount];
+}
+- (void) updateTables {
+    //this function gets called after the list is altered
+    [tableView1 reloadData];
+    [tableView2 reloadData];
+}
+- (void) updateCount {
+    //set the labels with the items count
+    //this function is called after the list is altered needing the text field to be cleared, table to be updated, and disable any buttons that require a row to be selected
+    list1LabelCount.text = [NSString stringWithFormat:@"Items(%lu)",(unsigned long)[self.nr_mut_array_list1 count]];
+    list2LabelCount.text = [NSString stringWithFormat:@"Items(%lu)",(unsigned long)[self.nr_mut_array_list2 count]];
+    [textField setText:@""];
+    [self updateTables];
+    [self disableButtons];
+    
+}
+- (void) disableButtons {
+    //a row must be selected in order for these 4 buttons to be enabled.  this function is called after the list is updated and the buttons need to become disabled
+    [addBeforeButton setEnabled:false];
+    [addAfterButton setEnabled:false];
+    [replaceButton setEnabled:false];
+    [removeButton setEnabled:false];
+}
+#pragma Table Methods
 -(NSInteger)tableView:(UITableView *)tableView
 numberOfRowsInSection:(NSInteger)section
-{return [self.nr_mut_array count];}
+{
+    if(tableView == tableView1){
+        return [self.nr_mut_array_list1 count];
+    }
+    else if(tableView == tableView2){
+        return [self.nr_mut_array_list2 count];
+    }
+    else{
+        return 0;
+    }
+}
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    //enable these buttons when a row is selected
+    [addBeforeButton setEnabled:true];
+    [addAfterButton setEnabled:true];
+    [replaceButton setEnabled:true];
+    [removeButton setEnabled:true];
+}
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    //when a row is selected again, that will deselect the row and we need the buttons disabled
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    if ([cell isSelected]) {
+        [tableView deselectRowAtIndexPath:indexPath animated:true];
+        [self disableButtons];
+        return nil;
+    }
+    
+    return indexPath;
+}
 
 -(UITableViewCell *)tableView:(UITableView *)tableView
         cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -47,70 +106,93 @@ numberOfRowsInSection:(NSInteger)section
     if(cell == nil)
     {
         cell = [[UITableViewCell alloc]initWithFrame:CGRectZero];//initWithFrame:CGRectZero
-                 
+        
     }
-    
-   cell.textLabel.text = [self.nr_mut_array objectAtIndex:indexPath.row];
+    if(tableView == tableView1){
+        cell.textLabel.text = [self.nr_mut_array_list1 objectAtIndex:indexPath.row];
+        
+    }
+    else if(tableView == tableView2){
+        cell.textLabel.text = [self.nr_mut_array_list2 objectAtIndex:indexPath.row];
+        
+    }
     return cell;
 }
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
+#pragma User Actions
 - (IBAction)addToList {
-    [self.nr_mut_array addObject:[textField text]];
-    [self addToView];
-  //  NSLog(@"%@",textField);
+    @synchronized(self){
+        [self.nr_mut_array_list1 addObject:[textField text]];
+        [self updateCount];
+    }
 }
 - (IBAction)clearList {
-    [self refreshArray];
-    [self addToView];
+    @synchronized(self){
+        [self refreshArray];
+        [self updateCount];
+    }
 }
 - (IBAction)removeRow {
-    NSIndexPath *selectedIndexPath = [tableView indexPathForSelectedRow];
-    [self.nr_mut_array removeObjectAtIndex:selectedIndexPath.row];
-    [self addToView];
-    [tableView reloadData];
+    @synchronized(self){
+        NSIndexPath *selectedIndexPath = [tableView1 indexPathForSelectedRow];
+        [self.nr_mut_array_list1 removeObjectAtIndex:selectedIndexPath.row];
+        [self updateCount];
+        [tableView1 reloadData];
+    }
 }
 - (IBAction)replaceRow {
-    NSIndexPath *selectedIndexPath = [tableView indexPathForSelectedRow];
-    [self.nr_mut_array setObject:textField.text atIndexedSubscript:selectedIndexPath.row];//removeObjectAtIndex:selectedIndexPath.row];
-    [self addToView];
-    [tableView reloadData];
+    @synchronized(self){
+        NSIndexPath *selectedIndexPath = [tableView1 indexPathForSelectedRow];
+        [self.nr_mut_array_list1 setObject:textField.text atIndexedSubscript:selectedIndexPath.row];
+        [self updateCount];
+        [tableView1 reloadData];
+    }
 }
 - (IBAction)addAfter {
-    NSIndexPath *selectedIndexPath = [tableView indexPathForSelectedRow];
-    [self.nr_mut_array insertObject:textField.text atIndex:selectedIndexPath.row+1];
- //   [self.nr_mut_array setObject:textField.text atIndexedSubscript:selectedIndexPath.row+1];//removeObjectAtIndex:selectedIndexPath.row];
-    [self addToView];
-    [tableView reloadData];
+    @synchronized(self){
+        NSIndexPath *selectedIndexPath = [tableView1 indexPathForSelectedRow];
+        [self.nr_mut_array_list1 insertObject:textField.text atIndex:selectedIndexPath.row+1];//add one to index to add after
+        [self updateCount];
+        [tableView1 reloadData];
+    }
 }
 - (IBAction)addBefore {
-    NSIndexPath *selectedIndexPath = [tableView indexPathForSelectedRow];
-    [self.nr_mut_array insertObject:textField.text atIndex:selectedIndexPath.row];
-    //   [self.nr_mut_array setObject:textField.text atIndexedSubscript:selectedIndexPath.row+1];//removeObjectAtIndex:selectedIndexPath.row];
-    [self addToView];
-    [tableView reloadData];
+    @synchronized(self){
+        NSIndexPath *selectedIndexPath = [tableView1 indexPathForSelectedRow];
+        [self.nr_mut_array_list1 insertObject:textField.text atIndex:selectedIndexPath.row];//no need to change index path as this will inser and move the chain behind this indexpath
+        [self updateCount];
+        [tableView1 reloadData];
+    }
 }
-- (void) refreshArray {
-    self.nr_mut_array = [[[NSMutableArray alloc] init]autorelease]; //auto release since ARC is turned off
-    [self.nr_mut_array addObject:@"Eric Enders"];
-     [self.nr_mut_array addObject:@"is"];
-     [self.nr_mut_array addObject:@"perfect"];
-     [self.nr_mut_array addObject:@"for"];
-     [self.nr_mut_array addObject:@"this"];
-     [self.nr_mut_array addObject:@"role"];
+- (IBAction) sortArray {
+    @synchronized(self){
+        [self.nr_mut_array_list1 sortUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+        [tableView1 reloadData];
+    }
 }
 - (IBAction) shuffleArray {
-    
-        NSUInteger count = [self.nr_mut_array count];
-        if (count <= 1) return;
-        for (NSUInteger i = 0; i < count - 1; ++i) {
+    @synchronized(self){
+        NSUInteger count = [self.nr_mut_array_list1 count]; //get list count
+        if (count <= 1) return; // no point in proceeding if list is empty
+        for (NSUInteger i = 0; i < count - 1; ++i) { //loop through randomizing the array but moving the indexes around
             NSInteger remainingCount = count - i;
             NSInteger exchangeIndex = i + arc4random_uniform((u_int32_t )remainingCount);
-            [self.nr_mut_array exchangeObjectAtIndex:i withObjectAtIndex:exchangeIndex];
+            [self.nr_mut_array_list1 exchangeObjectAtIndex:i withObjectAtIndex:exchangeIndex];
         }
-    [tableView reloadData];
+        [tableView1 reloadData];
+    }
+}
+- (IBAction) mergeLists {
+    //add objects from list one to list 2
+    //remove all of the objects from list 1
+    //sort list 2
+    //updateCount will update the labels for the count and also updates the tables by reloading the data
+    @synchronized(self){
+        [self.nr_mut_array_list2 addObjectsFromArray:self.nr_mut_array_list1];
+        [self.nr_mut_array_list1 removeAllObjects];
+        [self.nr_mut_array_list2 sortUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+        [self updateCount];
+    }
 }
 
 
